389c23f4147134e2cd871331465d2c86e52f73d3
Alexandre Sampaio <alexandre@globalnetsis.com.br>
2016-08-08 11:33:23 -0300

    Issue #18 ➤ Removendo debug provisorio em sessions controller

--- app/controllers/painel/masters/sessions_controller.rb
+++ app/controllers/painel/masters/sessions_controller.rb
@@ -9,7 +9,6 @@ class Painel::Masters::SessionsController < Devise::SessionsController
   # POST /resource/sign_in
   def create
     self.resource = warden.authenticate!(auth_options)
-    binding.pry
     set_flash_message(:notice, :signed_in) if is_navigational_format?
     sign_in(resource_name, resource)
     if !session[:return_to].blank?
