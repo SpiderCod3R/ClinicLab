34694fc56e7a410f0029fdc8affb17a3f44a9d26
Alexandre Sampaio <alexandre@globalnetsis.com.br>
2016-08-08 11:32:41 -0300

    Issue #18 ➤ Atualizando devise com timeout de 60 minutos

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -164,7 +164,7 @@ Devise.setup do |config|
   # ==> Configuration for :timeoutable
   # The time you want to timeout the user session without activity. After this
   # time the user will be asked for credentials again. Default is 30 minutes.
-  config.timeout_in = 30.minutes
+  config.timeout_in = 60.minutes
 
   # ==> Configuration for :lockable
   # Defines which strategy will be used to lock an account.
@@ -187,7 +187,7 @@ Devise.setup do |config|
   config.maximum_attempts = 10
 
   # Time interval to unlock the account if :time is enabled as unlock_strategy.
-  config.unlock_in = 10.minutess
+  config.unlock_in = 10.minutes
 
   # Warn on the last attempt before the account is locked.
   config.last_attempt_warning = true
