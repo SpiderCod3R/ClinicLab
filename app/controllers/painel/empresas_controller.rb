53643f7614318f34412d997fd9c6ed8f04405949
Alexandre Sampaio <alexandre@globalnetsis.com.br>
2016-08-09 14:24:03 -0300

    Issue #18 ➤ Atualizando o controller de empresas no painel administrativo
    Exemplos -> http://codeseven.github.io/toastr/demo.html
    Signed-off-by: Alexandre Sampaio <alexandre@globalnetsis.com.br>

--- app/controllers/painel/empresas_controller.rb
+++ app/controllers/painel/empresas_controller.rb
@@ -1,47 +1,67 @@
 class Painel::EmpresasController < ApplicationController
-  before_action :set_painel_empresa, only: [:show, :edit, :update, :destroy]
+  layout 'admin'
+  before_action :set_empresa, only: [:show, :edit, :update, :destroy]
 
   respond_to :html
 
   def index
-    @painel_empresas = Painel::Empresa.all
-    respond_with(@painel_empresas)
+    @empresas = Painel::Empresa.em_ordem_alfabetica.page params[:page]
+    respond_with(@empresas)
   end
 
   def show
-    respond_with(@painel_empresa)
+    @usuario = Painel::Usuario.new
+    @administradores = @empresa.administradores.page params[:administradores_pagina]
+    # @empresa_permissao = @empresa.empresa_permissoes.build
+    # @permissoes = @empresa.permissoes.page params[:permissoes]
+    # @funcionarios = @empresa.funcionarios.page params[:funcionarios_pagina]
+    respond_with(@empresa)
   end
 
   def new
-    @painel_empresa = Painel::Empresa.new
-    respond_with(@painel_empresa)
+    @empresa = Painel::Empresa.new
+    @empresa.usuarios.build
+    respond_with(@empresa)
   end
 
   def edit
   end
 
   def create
-    @painel_empresa = Painel::Empresa.new(painel_empresa_params)
-    @painel_empresa.save
-    respond_with(@painel_empresa)
+    @empresa = Painel::Empresa.new(empresa_params)
+    if @empresa.save
+      redirect_to painel_empresa_path(@empresa)
+    else
+      flash[:error] = "ERROR"
+      render :new
+    end
   end
 
   def update
-    @painel_empresa.update(painel_empresa_params)
-    respond_with(@painel_empresa)
+    @empresa.update(empresa_params)
+    respond_with(@empresa)
   end
 
   def destroy
-    @painel_empresa.destroy
-    respond_with(@painel_empresa)
+    @empresa.destroy
+    respond_with(@empresa)
   end
 
   private
-    def set_painel_empresa
-      @painel_empresa = Painel::Empresa.find(params[:id])
+    def set_empresa
+      @empresa = Painel::Empresa.find(params[:id])
     end
 
-    def painel_empresa_params
-      params.require(:painel_empresa).permit(:nome, :status)
+    def empresa_params
+      params.require(:painel_empresa).permit(:nome, :status,
+                                      usuarios_attributes: [:nome,
+                                                            :login,
+                                                            :email,
+                                                            :password,
+                                                            :password_confirmation,
+                                                            :admin,
+                                                            :telefone,
+                                                            :codigo_pais,
+                                                            :_destroy])
     end
 end
