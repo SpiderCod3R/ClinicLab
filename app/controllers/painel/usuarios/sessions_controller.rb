de29a73e28995fcdd4580b8805b303f0d5284d88
Alexandre Sampaio <alexandre@globalnetsis.com.br>
2016-08-10 10:56:31 -0300

    iSSUE[#18] ➤ reescrevendo o sessions controller para o usuario

--- app/controllers/painel/usuarios/sessions_controller.rb
+++ app/controllers/painel/usuarios/sessions_controller.rb
@@ -1,25 +1,33 @@
 class Painel::Usuarios::SessionsController < Devise::SessionsController
-# before_action :configure_sign_in_params, only: [:create]
+  before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  def new
+    super
+  end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    self.resource = warden.authenticate!(auth_options)
+    set_flash_message(:notice, :signed_in) if is_navigational_format?
+    sign_in(resource_name, resource)
+    if !session[:return_to].blank?
+     redirect_to session[:return_to]
+     session[:return_to] = nil
+    else
+     respond_with resource, :location => after_sign_in_path_for(resource)
+    end
+  end
 
   # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+  def destroy
+    super
+  end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+    def configure_sign_in_params
+      devise_parameter_sanitizer.permit(:sign_in, keys: [:login])
+    end
 end
