7acc03c1fc36c7ea0e865ae465e6f56c0df91089
Alexandre Sampaio <alexandre@globalnetsis.com.br>
2016-08-11 14:44:33 -0300

    iSSUE[#18] ➤ Adicionando timeout no GSUPER -> Master

--- app/models/painel/master.rb
+++ app/models/painel/master.rb
@@ -1,6 +1,5 @@
 class Painel::Master < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable,
+         :validatable, :timeoutable
 end
