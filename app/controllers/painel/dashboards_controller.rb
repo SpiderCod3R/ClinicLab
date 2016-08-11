081ca15338fae77e2b2935176c3ba3e23bb1fd4c
Alexandre Sampaio <alexandre@globalnetsis.com.br>
2016-08-11 14:44:07 -0300

    iSSUE[#18] ➤ Melhorando o Import das permissoes da empresa

--- app/controllers/painel/dashboards_controller.rb
+++ app/controllers/painel/dashboards_controller.rb
@@ -6,15 +6,21 @@ class Painel::DashboardsController < ApplicationController
   end
 
   def import_permissoes_to_company
+    @empresa = Painel::Empresa.find(params[:painel_empresa_permissao][:empresa_id])
     if params[:check_all]
-      @empresa = Painel::Empresa.find(params[:painel_empresa_permissao][:empresa_id])
       if @empresa.import_todas_permissoes
         flash[:info] = "Todas Permissoes vinculadas com sucesso."
       else
         flash[:warning] = "Permissoes não foram vinculadas!"
       end
     elsif params[:painel_empresa_permissao]
+      @empresa.import_permissoes(params[:painel_empresa_permissao])
     end
     redirect_back(fallback_location: @empresa)
   end
+
+  private
+    def resource_params
+      params.require(:empresa_permissao).permit(:empresa_id, permissao_ids: [])
+    end
 end
