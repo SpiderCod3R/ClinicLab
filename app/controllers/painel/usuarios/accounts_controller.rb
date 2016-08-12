8319554c5ff216775be9531fce3a942923d92007
Alexandre Sampaio <alexandre@globalnetsis.com.br>
2016-08-12 10:07:30 -0300

    ISSUE[#18] ➤ Adicionando mais funcionalidades ao usuario
    Signed-off-by: Alexandre Sampaio <alexandre@globalnetsis.com.br>

--- app/controllers/painel/usuarios/accounts_controller.rb
+++ app/controllers/painel/usuarios/accounts_controller.rb
@@ -1,8 +1,17 @@
 class Painel::Usuarios::AccountsController < ApplicationController
   before_action :authenticate_usuario!, only: [:index]
+  before_action :find_usuario, only: [:show_permissions, :change_password ,:destroy]
+
+  respond_to :html, :xml, :js, :json
 
   def index
     @empresa_usuarios = Painel::Usuario.where(empresa_id: params[:empresa_id]).page params[:page]
+  end
+
+  def show_permissions
+  end
+
+  def change_password
   end
 
   def new
