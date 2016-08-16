f86c2f40b4fc99b4d94654c06f247d1b2268eb0b
Alexandre Sampaio <alexandre@globalnetsis.com.br>
2016-08-16 08:20:38 -0300

    ISSUE[#18] ➤ Atualizando model painel/usuario_permissao
    Signed-off-by: Alexandre Sampaio <alexandre@globalnetsis.com.br>

--- app/models/painel/usuario_permissao.rb
+++ app/models/painel/usuario_permissao.rb
@@ -4,4 +4,8 @@ class Painel::UsuarioPermissao < ApplicationRecord
   belongs_to :permissao
 
   paginates_per 10
+
+   scope :permissoes_adicionadas, -> {
+    where("cadastrar = ? OR atualizar = ? OR exibir = ? OR deletar = ?", true, true, true, true)
+  }
 end
