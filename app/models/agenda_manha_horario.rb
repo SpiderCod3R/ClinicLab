e60c08ef68d7978cbf208df2c47edb3a231b0068
AlexandreGlobalnetsis <alexandre@globalnetsis.com.br>
2016-09-06 11:46:08 -0300

    Issue [#16] ➤ Criando metodos auxiliares da agenda_manha_horario
    Signed-off-by: AlexandreGlobalnetsis <alexandre@globalnetsis.com.br>

--- app/models/agenda_manha_horario.rb
+++ app/models/agenda_manha_horario.rb
@@ -5,4 +5,12 @@ class AgendaManhaHorario < ApplicationRecord
 
   DIA_DA_SEMANA = %w(Segunda-Feira Terça-Feira Quarta-Feira Quinta-Feira Sexta-Feira Sabado Domingo)
   TURNO = %W(Manha Tarde)
+
+  def profissional_nome
+    agenda.profissional_nome
+  end
+
+  def empresa_nome
+    agenda.empresa_nome
+  end
 end
