168bd42a6ee741203814e401b1a5d7430566ced9
AlexandreGlobalnetsis <alexandre@globalnetsis.com.br>
2016-09-13 17:44:38 -0300

    configurando deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,7 +18,7 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 set :puma_conf, "#{shared_path}/config/puma.rb"
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/rmweb-puma.sock"
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/gclinic-web-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
