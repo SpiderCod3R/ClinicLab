50389429c6fcafbe7c4c02f6288a3f87430beb23
AlexandreGlobalnetsis <alexandre@globalnetsis.com.br>
2016-09-13 14:15:10 -0300

    configurando deploy e production .rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,36 +1,49 @@
-# config valid only for current version of Capistrano
 lock '3.6.1'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :stage,     :production
+set :rails_env, :production
+set :application, 'rm-web'
+set :repo_url, 'git@gitlab.com:gclinic/gclinic2.0.git'
+set :user, 'rmweb'
+set :branch, 'master'
+set :deploy_to, "/home/#{fetch(:user)}/www/#{fetch(:application)}"
+set :scm, :git
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :format, :airbrussh
+set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'config/puma.rb')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
-# Default value for :scm is :git
-# set :scm, :git
+set :puma_conf, "#{shared_path}/config/puma.rb"
+set :puma_threads,    [4, 16]
+set :puma_workers,    0
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/rmweb-puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true
 
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
+set :pty, true
+set :keep_releases, 5
 
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
+set :ssh_options, {
+  forward_agent: true,
+  auth_methods: %w(publickey),
+  user: fetch(:user),
+}
 
-# Default value for :pty is false
-# set :pty, true
+before :deploy, "deploy:check_revision"
+before :deploy, "puma:make_dirs"
+after "assets:symlink", 'setup:database'
+after :deploy, 'deploy:cleanup'
 
-# Default value for :linked_files is []
-# append :linked_files, 'config/database.yml', 'config/secrets.yml'
+# namespace :deploy do
+# #  before 'check:linked_files', 'puma:config'
+# #  before 'check:linked_files', 'puma:nginx_config'
+# #  after 'puma:smart_restart', 'nginx:restart'
+# end
 
-# Default value for linked_dirs is []
-# append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
