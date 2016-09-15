943d60ce94e529dde6a3d7e934912cd6d0abb977
AlexandreGlobalnetsis <alexandre@globalnetsis.com.br>
2016-09-15 16:49:19 -0300

    Issue [#16] ➤ Atualizando agendas controller
    Signed-off-by: AlexandreGlobalnetsis <alexandre@globalnetsis.com.br>

--- app/controllers/painel/agendas_controller.rb
+++ app/controllers/painel/agendas_controller.rb
@@ -5,24 +5,16 @@ class Painel::AgendasController < ApplicationController
   respond_to :html, :js, :json, :xml
 
   def index
-    @agendas = Agenda.where(empresa_id: @empresa.id).order("created_at DESC")
+    @agendas = Agenda.where(empresa_id: @empresa.id)
   end
 
   def new
     @agenda = Agenda.new
-    7.times{|x| @agenda.agenda_manha_horarios.build}
-    7.times{|x| @agenda.agenda_tarde_horarios.build}
   end
 
   def create
-    @agenda = Agenda.new_by_javascript_params(params[:agenda])
-    @agenda.cria_horarios_turno_manha(params[:agenda][:horarios_manha])
-    # @agenda.cria_horarios_turno_tarde(params[:agenda][:horarios_tarde])
-    if @agenda.save
-      respond_to &:json
-    else
-      respond_to &:js
-    end
+    @agenda = Agenda.create_by_javascript_params(params[:agenda])
+    respond_to &:json
   end
 
   private
@@ -33,12 +25,9 @@ class Painel::AgendasController < ApplicationController
 
     def agenda_params
       params.require(:agenda).permit(:id, :empresa_id, :profissional_id,
-                                     :data_inicial, :data_final, :atendimento_sabado,
-                                     :atendimento_domingo, :horario_parcial,
+                                     :data, :atendimento_sabado,
+                                     :atendimento_domingo, :atendimento_parcial,
                                      :atendimento_manha_duracao, :atendimento_tarde_duracao,
-                                      agenda_manha_horarios_attributes: [ :dia, :turno, :inicio_do_atendimento, 
-                                                                 :final_do_atendimento, :agenda_id, :_destroy],
-                                      agenda_tarde_horarios_attributes: [ :dia, :turno, :inicio_do_atendimento, 
-                                                                 :final_do_atendimento, :agenda_id, :_destroy])
+                                     :atendimento_inicio, :atendimento_final)
     end
 end
