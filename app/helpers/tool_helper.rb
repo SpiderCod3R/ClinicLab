035f0bb885a1181b62ef02d4f9c21ac699e8a1cc
AlexandreGlobalnetsis <alexandre@globalnetsis.com.br>
2016-09-20 11:45:12 -0300

    Issue ➤ [#16] - Realocando helpers

--- app/helpers/tool_helper.rb
+++ app/helpers/tool_helper.rb
@@ -1,4 +1,33 @@
 module ToolHelper
+  def day_name(week_day)
+    Date::DAYNAMES[week_day]
+  end
+
+  def hora_formatada(hora)
+    hora.strftime("%H:%M") if hora.present?
+  end
+
+  def data_e_hora_formatada(data, hora)
+    if data.present? && hora.present?
+      content_tag(:label) do
+        data.strftime("%d/%m/%Y") + " - " + hora.strftime("%H:%M")
+      end
+    end
+  end
+
+  def ativo_ou_inativo?(resource)
+    return resource ? "ATIVO" : "INATIVO"
+  end
+
+  def sexo_formatado(s)
+    sexo = ''
+    if s == 'f'
+      sexo = 'Feminino'
+    elsif s == 'm'
+      sexo = 'Masculino'
+    end
+  end
+
   def novo
     t(:new, scope: [:bootstrap, :helpers, :links])
   end
