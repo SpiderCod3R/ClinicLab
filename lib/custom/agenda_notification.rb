98da3c8ce177c05bfbec6f69391e537dabe8e8c5
AlexandreGlobalnetsis <alexandre@globalnetsis.com.br>
2016-10-04 17:07:51 -0300

    Issue [#16] - Criando notificaçoes de exceçoes para a Agenda

---
+++ lib/custom/agenda_notification.rb
@@ -0,0 +1,32 @@
+class AgendaNotification < RuntimeError
+  attr_reader :error_messages, :resource, :error
+  attr_writer :error_messages
+
+  def initialize(resource, error_message="No value detected")
+    @resource = resource
+    @error_messages = Array.new
+    super(error_message)
+  end
+
+  def validate_shift_a!
+    begin
+      if resource[:horarios][:turno_a][:atendimentos].nil?
+        error_messages.push(I18n.t('agendas.messages.no_value_detected'))
+        raise
+      end
+    rescue => e
+      return @error_messages
+    end
+  end
+
+  def validate_shift_b!
+    begin
+      if resource[:horarios][:turno_b][:atendimentos].nil?
+        error_messages.push(I18n.t('agendas.messages.no_value_detected'))
+        raise
+      end
+    rescue => e
+      return @error_messages
+    end
+  end
+end
