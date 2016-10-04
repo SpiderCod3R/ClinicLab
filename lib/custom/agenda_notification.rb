3c00a162bf475b1fe2de0f0e597b889e522f5673
AlexandreGlobalnetsis <alexandre@globalnetsis.com.br>
2016-10-04 17:55:28 -0300

    Issue [#16] - Atualzizando validaçao das notificaçoes de horarios da Agenda

--- lib/custom/agenda_notification.rb
+++ lib/custom/agenda_notification.rb
@@ -10,23 +10,23 @@ class AgendaNotification < RuntimeError
 
   def validate_shift_a!
     begin
-      if resource[:horarios][:turno_a][:atendimentos].nil?
+      if resource[:horarios][:turno_a][:atendimentos].nil? && resource[:horarios][:turno_a][:atendimento_duracao].nil?
         error_messages.push(I18n.t('agendas.messages.no_value_detected'))
         raise
       end
     rescue => e
-      return @error_messages
+      return error_messages
     end
   end
 
   def validate_shift_b!
     begin
-      if resource[:horarios][:turno_b][:atendimentos].nil?
+      if resource[:horarios][:turno_b][:atendimentos].nil? && resource[:horarios][:turno_b][:atendimento_duracao].nil?
         error_messages.push(I18n.t('agendas.messages.no_value_detected'))
         raise
       end
     rescue => e
-      return @error_messages
+      return error_messages
     end
   end
 end
