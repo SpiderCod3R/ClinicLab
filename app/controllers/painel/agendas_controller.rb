e4569d72014a90654d7c4ef5d94369979c038fde
AlexandreGlobalnetsis <alexandre@globalnetsis.com.br>
2016-10-07 16:39:30 -0300

    Issue [#22] ➤ Melhorando Controller da Agenda + Criando Helper para a agenda

--- app/controllers/painel/agendas_controller.rb
+++ app/controllers/painel/agendas_controller.rb
@@ -5,13 +5,26 @@
 '''
 require 'custom/agenda_notification'
 class Painel::AgendasController < ApplicationController
+  include AgendasHelper
   before_action :authenticate_usuario!
   before_action :find_empresa
+  before_action :find_agenda, only: [:show, :movimentar, :destroy]
 
   respond_to :html, :js, :json, :xml
 
   def index
-    @agendas = Agenda.where(empresa_id: @empresa.id).page params[:page]
+    @search  = ransack_params
+    @search.build_sort if @search.sorts.empty?
+    @agendas = ransack_result
+  end
+
+  def advanced_search
+    index
+    render :index
+  end
+
+  def show
+    respond_with(@agenda)
   end
 
   def new
@@ -19,30 +32,43 @@ class Painel::AgendasController < ApplicationController
   end
 
   def create
-    @agenda_notification = AgendaNotification.new(params)
-    @invalid_fields_for_shift_a = @agenda_notification.validate_shift_a!
-    @invalid_fields_for_shift_b = @agenda_notification.validate_shift_b!
+    # @agenda_notification = AgendaNotification.new(params)
+    # @invalid_fields_for_shift_a = @agenda_notification.validate_shift_a!
+    # @invalid_fields_for_shift_b = @agenda_notification.validate_shift_b!
 
-    if @invalid_fields_for_shift_a.nil?
+    if params[:horarios][:turno_a][:atendimentos].present?
       @agenda_manha = Agenda.create_horarios_turno_a_by_javascript_params(params)
     end
 
-    if @invalid_fields_for_shift_b.nil?
+    if params[:horarios][:turno_b][:atendimentos].present?
       @agenda_tarde = Agenda.create_horarios_turno_b_by_javascript_params(params)
     end
   end
 
-  private
+  def destroy
+    if @agenda.destroy
+      flash[:warning] = "A agenda foi deletada."
+      redirect_to painel_empresa_agendas_path(@empresa)
+    else
+      flash[:error] = "A agenda não pôde ser deletada."
+      respond_with(@agenda)
+    end
+  end
 
+  private
     def find_empresa
       @empresa = Painel::Empresa.friendly.find(params[:empresa_id])
     end
 
-    def agenda_params
-      params.require(:agenda).permit(:id, :empresa_id, :profissional_id,
-                                     :data, :atendimento_sabado,
-                                     :atendimento_domingo, :atendimento_parcial,
-                                     :atendimento_duracao,
-                                     :atendimento_inicio, :atendimento_final)
+    def find_agenda
+      @agenda = Agenda.find_by(empresa_id: @empresa.id, id: params[:agenda_id])
+    end
+
+    def ransack_params
+      Agenda.includes(:profissional).ransack(params[:q])
+    end
+
+    def ransack_result
+      @search.result(distinct: agenda_wants_distinct_results?).where(empresa_id: @empresa.id).page(params[:page])
     end
 end
