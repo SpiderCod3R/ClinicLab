be1733cbcef4977c5a209242b0fff9649aeb72a0
AlexandreGlobalnetsis <alexandre@globalnetsis.com.br>
2016-10-07 16:31:00 -0300

    Issue [#22] ➤ Atualizando model cliente

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -12,11 +12,11 @@ class Cliente < ApplicationRecord
 
   usar_como_cpf :cpf
 
+  belongs_to :empresa, class_name: "Painel::Empresa", foreign_key: "empresa_id"
   belongs_to :estado
   belongs_to :cidade
   belongs_to :cargo
   belongs_to :convenio
-  belongs_to :empresa
 
   has_attached_file :foto, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :foto, content_type: /\Aimage\/.*\Z/
