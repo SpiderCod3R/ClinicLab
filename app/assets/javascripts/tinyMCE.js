856021950e30fbb56656280779a170e106c7dbbb
gabrielacabral <gabriela@globalnetsis.com.br>
2016-10-11 10:20:44 -0300

    Consertando erro no editor de texto 'tinymce' em producao: mudando configuracoes do tinyMCE.init em 'tinyMCE.js'

--- app/assets/javascripts/tinyMCE.js
+++ app/assets/javascripts/tinyMCE.js
@@ -1,3 +1,5 @@
  tinyMCE.init({
-    selector: 'textarea.editor'
+    selector: 'textarea.editor',
+    theme_url: (document.location.pathname + '/javascripts/TINYMCE/themes/modern/theme.js').replace("\/\/", "\/"),
+    skin_url:  (document.location.pathname + '/javascripts/TINYMCE/skins/lightgray/').replace("\/\/", "\/")
   });
