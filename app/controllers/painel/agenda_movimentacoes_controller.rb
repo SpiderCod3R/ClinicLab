3a6d93b1c1b33b34473d62a30e7a66bdd4cca7c5
AlexandreGlobalnetsis <alexandre@globalnetsis.com.br>
2016-10-11 11:07:27 -0300

    Issue [#22] ➤ Atualizando agenda_movimentacoes controller

--- app/controllers/painel/agenda_movimentacoes_controller.rb
+++ app/controllers/painel/agenda_movimentacoes_controller.rb
@@ -7,18 +7,50 @@ class Painel::AgendaMovimentacoesController < ApplicationController
   before_action :authenticate_usuario!
   before_action :find_empresa
   before_action :find_agenda
-  before_action :set_clientes, :set_convenios, only: [:new]
+  before_action :find_agenda_movimentacao, only: [:verify, :edit, :update]
+  before_action :set_clientes, :set_convenios, only: [:verify]
+
+  def verify
+    unless @movimentacao.nil?
+      render action: :edit
+    else
+      @movimentacao = AgendaMovimentacao.new
+      render action: :new
+    end
+  end
 
   def new
-    @nova_movimentacao = AgendaMovimentacao.new
-    respond_to &:js
+  end
+
+  def edit
   end
 
   def create
-    
+    @movimentacao = AgendaMovimentacao.build_movimentacao(params[:agenda_movimentacao])
+    @movimentacao.atendente = current_usuario
+    if @movimentacao.save
+      flash[:notice] = "Agenda movimentada com sucesso"
+      redirect_to painel_empresa_agenda_path(@empresa, @agenda)
+    else
+      render :new
+    end
+  end
+
+  def update
+    @movimentacao.atendente = current_usuario
+    if @movimentacao.update_movimentacao(params[:agenda_movimentacao])
+      flash[:notice] = "Agenda atualizada com sucesso"
+      redirect_to painel_empresa_agenda_path(@empresa, @agenda)
+    else
+      render :edit
+    end
   end
 
   private
+    def find_agenda_movimentacao
+      @movimentacao = AgendaMovimentacao.find_by(agenda_id: @agenda.id)
+    end
+
     def set_clientes
       @clientes = Cliente.where(empresa_id: @empresa.id)
     end
@@ -36,6 +68,15 @@ class Painel::AgendaMovimentacoesController < ApplicationController
     end
 
     def resource_params
-      params.require(:agenda_movimantacao).permit(:agenda_id, :convenio_id, :sem_convenio, :obeservacoes, :confirmacao)
+      params.require(:agenda_movimantacao).permit(:agenda_id,
+                                                  :convenio_id,
+                                                  :sem_convenio,
+                                                  :obeservacoes,
+                                                  :confirmacao,
+                                                  :nome_paciente,
+                                                  :telefone_paciente,
+                                                  :email_paciente,
+                                                  :solicitante_id,
+                                                  :id_paciente)
     end
 end
