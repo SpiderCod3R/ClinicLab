9a26c1868dfe12d79a856bf752870041bcc20934
AlexandreGlobalnetsis <alexandre@globalnetsis.com.br>
2016-10-11 11:17:08 -0300

    Issue [#22] ➤ Atualizando search_controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,7 @@ class SearchController < ApplicationController
   skip_authorize_resource
   def buscar_pacientes
     if params[:nome_paciente]  != ""
-      @pacientes = Paciente.where("nome LIKE ?", "#{params[:nome_paciente]}%").where(empresa_id: current_usuario.empresa_id).take(10)
+      @pacientes = Cliente.where("nome LIKE ?", "#{params[:nome_paciente]}%").where(empresa_id: current_usuario.empresa_id).take(10)
     end
   end
 end
