0fd5d2008703ac6b919ac8f43aecf0fd6a2e4256
AlexandreGlobalnetsis <alexandre@globalnetsis.com.br>
2016-12-07 09:45:09 -0200

    Issue[#27] ➤ Iniciando procedimento de permissoes para a agenda

--- app/models/painel/usuario_permissao.rb
+++ app/models/painel/usuario_permissao.rb
@@ -5,7 +5,13 @@ class Painel::UsuarioPermissao < ApplicationRecord
 
   paginates_per 10
 
-   scope :permissoes_adicionadas, -> {
+  scope :permissoes_adicionadas, -> {
     where("cadastrar = ? OR atualizar = ? OR exibir = ? OR deletar = ?", true, true, true, true)
   }
+
+  def self.possuiAgenda?
+    @agenda = Painel::Permissao.find_by(model_class: "Agenda")
+    @permissao = where(permissao: @agenda)
+    return true if !@permissao.nil?
+  end
 end
