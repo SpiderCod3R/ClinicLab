52d4e723de9223b8f5990d93fc4c87ba18a47f55
AlexandreGlobalnetsis <alexandre@globalnetsis.com.br>
2016-12-07 10:08:14 -0200

    Issue[#27] ➤ Melhorando o metodo para verificar se o usuario possui agenda
    Signed-off-by: AlexandreGlobalnetsis <alexandre@globalnetsis.com.br>

--- app/models/painel/usuario_permissao.rb
+++ app/models/painel/usuario_permissao.rb
@@ -9,9 +9,14 @@ class Painel::UsuarioPermissao < ApplicationRecord
     where("cadastrar = ? OR atualizar = ? OR exibir = ? OR deletar = ?", true, true, true, true)
   }
 
+  '''
+    METODO PARA VERIFICAR SE O USUARIO POSSUI Agenda
+    EM SUAS MODALIDADES PERMITIDAS
+  '''
   def self.possuiAgenda?
-    @agenda = Painel::Permissao.find_by(model_class: "Agenda")
-    @permissao = where(permissao: @agenda)
-    return true if !@permissao.nil?
+    @agenda_permissao = Painel::Permissao.find_by(model_class: "Agenda")
+    @permissao = where(permissao_id: @agenda_permissao.id)
+    @permissao = @permissao.permissoes_adicionadas
+    return true if !@permissao.empty?
   end
 end
