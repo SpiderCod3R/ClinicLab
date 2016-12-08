ecf727226c5b0f1da2060368f6bdb98bb9b2d24b
AlexandreGlobalnetsis <alexandre@globalnetsis.com.br>
2016-12-08 13:56:01 -0200

    ajeitando rota da ficha do paciente(cliente)

--- config/routes.rb
+++ config/routes.rb
@@ -39,6 +39,7 @@ Rails.application.routes.draw do
   get 'relatorios/new' => "configuracao_relatorios#new"
   get 'conselhos_regionais/new' => "conselho_regionais#new"
 
+  post 'agendas/clientes/new_paciente', to: "clientes#change_or_create_new_paciente", as: :change_or_create_new_paciente
   post 'clientes/retorna_historico', to: "clientes#retorna_historico"
   post 'clientes/salva_historico', to: "clientes#salva_historico"
   post 'clientes/atualiza_historico', to: "clientes#atualiza_historico"
@@ -60,6 +61,7 @@ Rails.application.routes.draw do
   get 'search/find-texto-livres'=> "search#collect_all_free_text" ,as: :collect_all_free_text
   get 'search/conselho_regional', to: 'conselho_regionais#search'
   get 'search/cliente-texto-livre', to: 'search#find_cliente_texto_livre'
+
 
   post 'clientes/include_texto_livre', to: 'clientes#include_texto_livre'
 
