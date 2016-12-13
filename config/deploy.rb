5b1009eeb487efd182ea0f0b6fc46f7bb3a23e9b
AlexandreGlobalnetsis <alexandre@globalnetsis.com.br>
2016-12-13 15:26:36 -0200

    configuração nova

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@ lock '3.6.1'
 
 set :stage,     :production
 set :rails_env, :production
-set :application, 'gclinic-web'
+set :application, 'gclinic'
 set :repo_url, 'git@gitlab.com:gclinic/gclinic2.0.git'
 set :user, 'deployer'
 set :branch, 'master'
@@ -18,7 +18,7 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 set :puma_conf, "#{shared_path}/config/puma.rb"
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/gclinic-web-puma.sock"
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/gclinic_puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
