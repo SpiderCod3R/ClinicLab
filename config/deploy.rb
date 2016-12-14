f91774bf30ed9bda619076d699d27978b9819cfd
AlexandreGlobalnetsis <alexandre@globalnetsis.com.br>
2016-12-14 08:57:22 -0200

    deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@ lock '3.6.1'
 
 set :stage,     :production
 set :rails_env, :production
-set :application, 'gclinic'
+set :application, 'rm-web'
 set :repo_url, 'git@gitlab.com:gclinic/gclinic2.0.git'
 set :user, 'deployer'
 set :branch, 'master'
@@ -15,9 +15,9 @@ set :format_options, command_output: true, log_file: 'log/capistrano.log', color
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'config/puma.rb')
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
+set :puma_conf, "#{shared_path}/config/puma.rb"
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
-set :puma_conf, "#{shared_path}/config/puma.rb"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
@@ -33,51 +33,10 @@ set :keep_releases, 5
 set :ssh_options, {
   forward_agent: true,
   auth_methods: %w(publickey),
-  user: fetch(:user),
-  keys: %w(~/.ssh/id_rsa.pub)
+  user: fetch(:user)
 }
 
-namespace :puma do
-  desc 'Create Directories for Puma Pids and Socket'
-  task :make_dirs do
-    on roles(:app) do
-      execute "mkdir #{shared_path}/tmp/sockets -p"
-      execute "mkdir #{shared_path}/tmp/pids -p"
-    end
-  end
-
-  before :start, :make_dirs
-end
-
-namespace :deploy do
-  desc "Make sure local git is in sync with remote."
-  task :check_revision do
-    on roles(:app) do
-      unless `git rev-parse HEAD` == `git rev-parse origin/master`
-        puts "WARNING: HEAD is not the same as origin/master"
-        puts "Run `git push` to sync changes."
-        exit
-      end
-    end
-  end
-
-  desc 'Initial Deploy'
-  task :initial do
-    on roles(:app) do
-      before 'deploy:restart', 'puma:start'
-      # invoke 'deploy'
-    end
-  end
-
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      invoke 'puma:restart'
-    end
-  end
-
-  before :starting,     :check_revision
-  after  :finishing,    :compile_assets
-  after  :finishing,    :cleanup
-  # after  :finishing,    :restart
-end
+before :deploy, "deploy:check_revision"
+before :deploy, "puma:make_dirs"
+after "assets:symlink", 'setup:database'
+after :deploy, 'deploy:cleanup'
