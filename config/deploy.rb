bfb01bf898c920a4f5e80e89b6f4f155fa405dca
AlexandreGlobalnetsis <alexandre@globalnetsis.com.br>
2016-12-14 16:38:14 -0200

    config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,27 @@
 lock '3.6.1'
 
+set :port, 49697
+set :user, 'deployer'
+set :deploy_via, :remote_cache
+set :use_sudo, false
+
+server '138.197.135.242',
+  roles: [:web, :app, :db],
+  port: fetch(:port),
+  user: fetch(:user),
+  primary: true
+
+set :ssh_options, {
+  forward_agent: true,
+  auth_methods: %w(publickey),
+  user: fetch(:user),
+}
+
+set :rbenv_ruby, '2.3.1'
+set :rbenv_type, :user
+
+set :conditionally_migrate, true
+
 set :stage,     :production
 set :rails_env, :production
 set :application, 'gclinic'
