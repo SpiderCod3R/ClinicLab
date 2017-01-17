a21725950d44aba1426c538890c7ec5e1c0f0535
AlexandreGlobalnetsis <alexandre@globalnetsis.com.br>
2017-01-17 17:39:38 -0200

    Adicionando filtros em Imagem Cabecs

--- app/models/imagem_cabec.rb
+++ app/models/imagem_cabec.rb
@@ -1,6 +1,11 @@
 class ImagemCabec < ApplicationRecord
-  include Paperclip::Glue
   validates :imagem, :nome,  presence: true
   has_attached_file :imagem, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :imagem, content_type: /\Aimage\/.*\Z/
+
+  RANSACKABLE_ATTRIBUTES = ["nome"]
+
+  def self.ransackable_attributes auth_object = nil
+    (RANSACKABLE_ATTRIBUTES) + _ransackers.keys
+  end
 end
