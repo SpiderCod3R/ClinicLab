3978f909be4b56805896eb5717d4a19e0e9c39ff
Alexandre Sampaio <alexandre@globalnetsis.com.br>
2017-02-07 14:42:50 -0200

    Issue #32 criando diretorio com migrations compartilhadas

--- db/migrate/20160805141154_add_nome_login_to_painel_masters.rb
+++
@@ -1,6 +0,0 @@
-class AddNomeLoginToPainelMasters < ActiveRecord::Migration[5.0]
-  def change
-    add_column :painel_masters, :nome, :string
-    add_column :painel_masters, :login, :string
-  end
-end
