177b99accfe1781a7bfc6ddbe5418e7c4aea5b7a
Alexandre Sampaio <alexandre@globalnetsis.com.br>
2017-02-10 17:53:40 -0200

    Issue #32 > Atualizando ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,31 +2,31 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user ||= Painel::Usuario.new
+    user ||= Gclinic::User.new
 
-    if can :read, Painel::Empresa do |e|
+    if can :read, Empresa do |e|
         e.try(:id).eql?(user.empresa_id)
       end
     end
 
     if user.admin?
-      can :manage, Painel::Usuario
+      can :manage, Gclinic::User
 
-      user.empresa.try(:empresa_permissoes).each do |f|
-        if f.try(:empresa_id).eql?(user.empresa_id)
-          user.empresa.try(:permissoes).each do |p|
+      user.environment.try(:environment_models).each do |f|
+        if f.try(:environment_id).eql?(user.environment_id)
+          user.environment.try(:models).each do |p|
             can :manage, p.model_class.constantize
           end
         end
       end
-    elsif user.funcionario?
-      user.empresa.try(:empresa_permissoes).each do |f|
-        if f.try(:empresa_id).eql?(user.empresa_id)
-          user.try(:usuario_permissoes).each do |p|
-            can :create,  p.permissao.model_class.constantize if p.cadastrar?
-            can :update,  p.permissao.model_class.constantize if p.atualizar?
-            can :read,    p.permissao.model_class.constantize if p.exibir?
-            can :destroy, p.permissao.model_class.constantize if p.deletar?
+    elsif user.employee?
+      user.environment.try(:environment_models).each do |f|
+        if f.try(:environment_id).eql?(user.environment_id)
+          user.try(:user_models).each do |p|
+            can :create,  p.model.model_class.constantize if p.cadastrar?
+            can :update,  p.model.model_class.constantize if p.atualizar?
+            can :read,    p.model.model_class.constantize if p.exibir?
+            can :destroy, p.model.model_class.constantize if p.deletar?
           end
         end
       end
