96fb43b0b7df20dfb09325b91ddada8867c040ca
Alexandre Sampaio <alexandre@globalnetsis.com.br>
2017-02-13 15:13:10 -0200

    Issue #32 > Adiciaonando InsideController em Profissionais
    Signed-off-by: Alexandre Sampaio <alexandre@globalnetsis.com.br>

--- app/controllers/profissionais_controller.rb
+++ app/controllers/profissionais_controller.rb
@@ -1,4 +1,4 @@
-class ProfissionaisController < ApplicationController
+class ProfissionaisController < Support::InsideController
   before_action :set_estados, only: [:new, :edit, :create, :update]
   before_action :set_conselhos_regionais, only: [:new, :edit, :create, :update]
   before_action :set_operadoras, only: [:new, :edit, :create, :update]
@@ -7,9 +7,9 @@ class ProfissionaisController < ApplicationController
 
   def index
     if params[:status] || params[:cargo] || params[:nome]
-      @profissionais = Profissional.where(empresa_id: current_usuario.empresa_id).search(params[:status]['status'], params[:cargo]['id'], params[:nome]).order("created_at DESC").page(params[:page]).per(10)
+      @profissionais = Profissional.where(empresa: current_user.empresa).search(params[:status]['status'], params[:cargo]['id'], params[:nome]).order("created_at DESC").page(params[:page]).per(10)
     else
-      @profissionais = Profissional.where(empresa_id: current_usuario.empresa_id).order("created_at DESC").page(params[:page]).per(10)
+      @profissionais = Profissional.where(empresa: current_user.empresa).order("created_at DESC").page(params[:page]).per(10)
       respond_with(@profissionais)
     end
   end
@@ -19,7 +19,7 @@ class ProfissionaisController < ApplicationController
   end
 
   def new
-    @profissional = current_usuario.empresa.profissionais.build
+    @profissional = current_user.empresa.profissionais.build
     respond_with(@profissional)
   end
 
@@ -27,10 +27,10 @@ class ProfissionaisController < ApplicationController
   end
 
   def create
-    @profissional = current_usuario.empresa.profissionais.build(resource_params)
+    @profissional = current_user.empresa.profissionais.build(resource_params)
     if @profissional.save
       flash[:success] = t("flash.actions.#{__method__}.success", resource_name: @profissional.class)
-      redirect_to new_profissional_path
+      redirect_to new_empresa_profissional_path(current_user.empresa)
     else
       flash[:error] = t("flash.actions.#{__method__}.alert", resource_name: @profissional.class)
       render :new
@@ -38,10 +38,9 @@ class ProfissionaisController < ApplicationController
   end
 
   def update
-    # binding.pry
     if @profissional.update(resource_params)
       flash[:success] = t("flash.actions.#{__method__}.success", resource_name: @profissional.class)
-      redirect_to profissionais_path
+      redirect_to empresa_profissional_path(current_user.empresa, @profissional)
     else
       flash[:error] = t("flash.actions.#{__method__}.alert", resource_name: @profissional.class)
       render :edit
@@ -51,7 +50,7 @@ class ProfissionaisController < ApplicationController
   def destroy
     if @profissional.destroy
       flash[:success] = t("flash.actions.#{__method__}.success", resource_name: @profissional.class)
-      redirect_to profissionais_path
+      redirect_to empresa_profissionais_path(current_user.empresa)
     else
       flash[:error] = t("flash.actions.#{__method__}.alert", resource_name: @profissional.class)
     end
@@ -63,7 +62,7 @@ class ProfissionaisController < ApplicationController
     end
 
     def set_cargos
-      @cargos = Cargo.da_empresa_atual(current_usuario.empresa).pelo_nome
+      @cargos = current_user.empresa.cargos.by_name
     end
 
     def set_estados
@@ -71,11 +70,11 @@ class ProfissionaisController < ApplicationController
     end
 
     def set_operadoras
-      @operadoras = Operadora.pelo_nome
+      @operadoras = current_user.empresa.operadoras.by_name
     end
 
     def set_conselhos_regionais
-      @conselhos_regionais = ConselhoRegional.pela_sigla
+      @conselhos_regionais = current_user.empresa.conselho_regionais.pela_sigla
     end
 
     def resource_params
