dc1f64a0eb9be517fb9163878bb0289325c6d564
Alexandre Sampaio <alexandre@globalnetsis.com.br>
2017-02-13 08:57:54 -0200

    Issue #32 > Atualizando Centro de Custos controller com current_user e Inside Controller

--- app/controllers/centro_de_custos_controller.rb
+++ app/controllers/centro_de_custos_controller.rb
@@ -1,11 +1,11 @@
-class CentroDeCustosController < ApplicationController
+class CentroDeCustosController < Support::InsideController
   load_and_authorize_resource param_method: :resource_params
   before_action :set_centro_de_custo, only: [:show, :edit, :update, :destroy]
 
   respond_to :html
 
   def index
-    @search = CentroDeCusto.where(empresa_id: current_usuario.empresa_id).ransack(params[:q])
+    @search = CentroDeCusto.where(empresa_id: current_user.empresa_id).ransack(params[:q])
     @centro_de_custos = @search.result.order("id desc").page(params[:page]).per(10)
     @search.build_condition if @search.conditions.empty?
   end
@@ -15,7 +15,7 @@ class CentroDeCustosController < ApplicationController
   end
 
   def new
-    @centro_de_custo = current_usuario.empresa.centro_de_custos.build
+    @centro_de_custo = current_user.empresa.centro_de_custos.build
     respond_with(@centro_de_custo)
   end
 
@@ -23,7 +23,7 @@ class CentroDeCustosController < ApplicationController
   end
 
   def create
-    @centro_de_custo = current_usuario.empresa.centro_de_custos.build(resource_params)
+    @centro_de_custo = current_user.empresa.centro_de_custos.build(resource_params)
     if @centro_de_custo.save
       flash[:success] = t("flash.actions.#{__method__}.success", resource_name: @centro_de_custo.class)
       redirect_to new_centro_de_custo_path
