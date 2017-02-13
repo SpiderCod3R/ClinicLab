c67810ed6cbc26e135679023db9526166746dbc7
Alexandre Sampaio <alexandre@globalnetsis.com.br>
2017-02-13 08:50:42 -0200

    Issue #32 > Adaptando Operadoras Controller com Controller Inside e current_user

--- app/controllers/operadoras_controller.rb
+++ app/controllers/operadoras_controller.rb
@@ -1,27 +1,25 @@
-class OperadorasController < ApplicationController
+class OperadorasController < Support::InsideController
   load_and_authorize_resource
   before_action :find_operadora, except: [:index, :new, :create]
 
   def index
-    @search = Operadora.where(empresa_id: current_usuario.empresa_id).ransack(params[:q])
+    @search = Operadora.where(empresa_id: current_user.empresa_id).ransack(params[:q])
     @operadoras = @search.result.order("id desc").page(params[:page]).per(10)
     @search.build_condition if @search.conditions.empty?
   end
 
   def show
-    #find_operadora
   end
 
   def new
-    @operadora = current_usuario.empresa.operadoras.build
+    @operadora = current_user.empresa.operadoras.build
   end
 
   def edit
-    #find_operadora
   end
 
   def create
-    @operadora = current_usuario.empresa.operadoras.build(resource_params)
+    @operadora = current_user.empresa.operadoras.build(resource_params)
     if @operadora.save
       redirect_to new_operadora_path, notice: t("flash.actions.#{__method__}.notice", resource_name: @operadora.nome)
     else
@@ -30,7 +28,6 @@ class OperadorasController < ApplicationController
   end
 
   def update
-    #find_operadora
     if @operadora.update(resource_params)
       redirect_to operadoras_path, notice: t("flash.actions.#{__method__}.notice", resource_name: @operadora.nome)
     else
@@ -47,12 +44,11 @@ class OperadorasController < ApplicationController
   end
 
   private
+    def find_operadora
+      @operadora = Operadora.find(params[:id])
+    end
 
-  def find_operadora
-    @operadora = Operadora.find(params[:id])
-  end
-
-  def resource_params
-    params.require(:operadora).permit(:nome, :status)
-  end
+    def resource_params
+      params.require(:operadora).permit(:nome, :status)
+    end
 end
