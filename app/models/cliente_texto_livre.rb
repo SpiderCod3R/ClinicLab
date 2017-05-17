88518567ab552538da8f5ea1aa0a624ff611d6e5
Alexandre Sampaio <alexandre@globalnetsis.com.br>
2017-05-17 17:47:45 -0300

     [Issue #41] -  Atualizando modelo cliente texto livre

--- app/models/cliente_texto_livre.rb
+++ app/models/cliente_texto_livre.rb
@@ -5,24 +5,15 @@ class ClienteTextoLivre < Connection::Factory
   belongs_to :texto_livre
   paginates_per 1
 
-
   class << self
     def include(resource)
       create!(cliente_id: resource[:cliente_id],
-              content_data: resource[:content],
+              content_data: resource[:content_data],
               texto_livre_id: resource[:texto_livre_id])
     end
   end
 
   def update_content(resource)
-    update_attributes(content_data: resource[:texto_livre][:content])
-  end
-
-  def next
-    self.class.where("id > ? AND cliente_id = ?", id, cliente_id).first
-  end
-
-  def previous
-    self.class.where("id < ? AND cliente_id = ?", id, cliente_id).last
+    update_attributes(content_data: resource[:content_data])
   end
 end
