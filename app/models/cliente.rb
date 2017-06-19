79222994f0f4f2d21f6f4da02d13fc18bcc61651
Alexandre Sampaio <alexandre@globalnetsis.com>
2017-06-19 10:41:47 -0300

    Correcao de Alguma coisa

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -16,7 +16,7 @@ class Cliente < Connection::Factory
 
   attr_accessor :receituario, :empresa_name
 
-  validates :cpf, presence: true, uniqueness: true
+  validates :cpf, cpf: true, presence: true, uniqueness: true
   validates :rg, uniqueness: true, presence: true
 
   belongs_to :empresa
