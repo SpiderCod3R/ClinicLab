d8a54e5b825a59e36af34470e42e5b8fa3e3ccf4
gabrielacabral <gabriela@globalnetsis.com.br>
2017-10-06 14:36:09 -0300

    Issue #43 - Removendo obrigatoriedade de campos 'tabela' e 'codigo_procedimento' em ExameProcedimento

--- app/models/exame_procedimento.rb
+++ app/models/exame_procedimento.rb
@@ -5,7 +5,7 @@ class ExameProcedimento < Connection::Factory
   has_many :grupo_exame_procedimentos
   has_many :grupos, through: :grupo_exame_procedimentos
 
-  validates_presence_of :descricao, :tabela, :codigo_procedimento
+  validates_presence_of :descricao
   validates :tabela, length: { maximum: 2 }
   validates :codigo_procedimento, length: { maximum: 10 }
 
