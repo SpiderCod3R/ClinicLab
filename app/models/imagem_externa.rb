883076b344720904fd0c0d8924c0fcdc5c70aa05
Alexandre Sampaio <alexandre@globalnetsis.com.br>
2017-11-08 11:27:49 -0200

    Adicionando validações em Imagens Externas

--- app/models/imagem_externa.rb
+++ app/models/imagem_externa.rb
@@ -9,8 +9,15 @@ class ImagemExterna < Connection::Factory
     end
   end
 
-  has_attached_file :foto_antes, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :foto_antes, content_type: /\Aimage\/.*\Z/
-  has_attached_file :foto_depois, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :foto_depois, content_type: /\Aimage\/.*\Z/
+  has_attached_file :foto_antes
+  has_attached_file :foto_depois
+
+  validates_with AttachmentSizeValidator, attributes: :foto_antes, less_than: 10.megabytes
+  validates_with AttachmentSizeValidator, attributes: :foto_depois, less_than: 10.megabytes
+
+  validates_attachment :foto_antes, content_type: { content_type: ["image/jpeg", "image/jpg", "image/png"], message: "Esta imagem não possui um formato válido. Apenas JPEG - JPG - PNG" }
+  validates_attachment :foto_depois, content_type: { content_type: ["image/jpeg", "image/jpg", "image/png"], message: "Esta imagem não possui um formato válido. Apenas JPEG - JPG - PNG" }
+
+  validates_attachment_file_name :foto_antes, matches: [/png\z/, /jpe?g\z/, /jp?g\z/], message: "Esta imagem não possui um formato válido. Apenas JPEG - JPG - PNG"
+  validates_attachment_file_name :foto_depois, matches: [/png\z/, /jpe?g\z/, /jp?g\z/], message: "Esta imagem não possui um formato válido. Apenas JPEG - JPG - PNG"
 end
